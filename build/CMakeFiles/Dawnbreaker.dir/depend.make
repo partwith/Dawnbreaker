0a51805fd3e42527ab2a21a4c8f191d1e238b8c6
Jizhe Hou <jizhehou@gmail.com>
2022-06-03 14:08:59 +1000

    remove build folder

--- build/CMakeFiles/Dawnbreaker.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Dawnbreaker.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/PartForYou.dir/src/PartForYou/GameObjects.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/PartForYou.dir/src/PartForYou/GameObjects.cpp.o: \
-  /Users/jizhehou/Documents/savvy/c++/CS100-HW89/src/PartForYou/GameObjects.cpp
